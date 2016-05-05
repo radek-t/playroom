06eccc57c1cc70b8cbcaa96bf84f089e4a2a8e52
radek-t <radek@zacnie.net>
2016-05-05 17:38:10 +0200

    More covered array functions

--- node/src/ArrayOps.js
+++ node/src/ArrayOps.js
@@ -1,14 +1,36 @@
 function ArrayOps() {
 }
 
-ArrayOps.prototype.sumArray = function(array){
-    if(!Array.isArray(array)) return false;
+ArrayOps.prototype.sumArray = function(array) {
+    if(!Array.isArray(array)) {
+        return false;
+    }
     
     var sum = array.length == 0 || typeof array[0] == 'number' ? 0 : '';
-    array.map(function(item){
+    array.map(function(item) {
         sum += Array.isArray(item) ? ArrayOps.prototype.sumArray(item) : item;
     })
     return sum;
+}
+
+ArrayOps.prototype.countUnique = function(array) {
+    if(!Array.isArray(array)) return 0;
+        
+    var counter = array.length, visited = {};
+    for (var i = 0 ; i < array.length ; i++ ) { 
+        var el = array[i];
+        el = typeof el == "object" ? JSON.stringify(el) : el;
+        if(visited[el] != undefined) {
+            counter--;
+            if(visited[el] === false) {
+                counter--;
+                visited[el] = true;
+            }
+        } else {
+            visited[el] = false;
+        }        
+    }
+    return counter;
 }
 
 module.exports = ArrayOps;
--- node/test/spec/ArrayOpsSpec.js
+++ node/test/spec/ArrayOpsSpec.js
@@ -4,6 +4,7 @@
   var ArrayOps = require('../../src/ArrayOps'), arrayOps;
 
   describe('ArrayOps module suite', function () {
+    
     describe('sumArray function', function () {
       
       it('should sum array with integers correctly', function () {
@@ -31,6 +32,75 @@
         expect(arrayOps.sumArray([])).toEqual(0);
       });
       
+    });
+    
+    describe('countUnique function', function () {
+      
+      it('should handle an empty array correctly', function () {
+        arrayOps = new ArrayOps();        
+        expect(arrayOps.countUnique([])).toEqual(0);
+      });
+      
+      it('should handle a single value array correctly', function () {
+        arrayOps = new ArrayOps();        
+        expect(arrayOps.countUnique([1])).toEqual(1);
+      });
+      
+      it('should handle a simple int array [1,2,3,2] correctly', function () {
+        arrayOps = new ArrayOps();        
+        expect(arrayOps.countUnique([1,2,3,2])).toEqual(2);
+      });
+      
+      it('should handle a simple unique int array [1,2,3,4] correctly', function () {
+        arrayOps = new ArrayOps();        
+        expect(arrayOps.countUnique([1,2,3,4])).toEqual(4);
+      });
+      
+      it('should handle multiple same value arrays [1,1,1,1] correctly', function () {
+        arrayOps = new ArrayOps();        
+        expect(arrayOps.countUnique([1,1,1,1])).toEqual(0);
+      });
+      
+      it('should handle a single string array correctly', function () {
+        arrayOps = new ArrayOps();        
+        expect(arrayOps.countUnique(['a'])).toEqual(1);
+      });
+      
+      it('should handle multiple same string arrays correctly', function () {
+        arrayOps = new ArrayOps();        
+        expect(arrayOps.countUnique(['a','a','a','a'])).toEqual(0);
+      });
+      
+      it('should handle a simple unique string array  correctly', function () {
+        arrayOps = new ArrayOps();        
+        expect(arrayOps.countUnique(['a','b','c','d'])).toEqual(4);
+      });
+      
+      it('should handle a simple string array  correctly', function () {
+        arrayOps = new ArrayOps();        
+        expect(arrayOps.countUnique(['a','b','d','d'])).toEqual(2);
+      });
+      
+      it('should handle a single object array correctly', function () {
+        arrayOps = new ArrayOps();        
+        expect(arrayOps.countUnique([{a:'asd'}])).toEqual(1);
+      });
+      
+      it('should handle a simple object array correctly', function () {
+        arrayOps = new ArrayOps();        
+        expect(arrayOps.countUnique([{a:'asd'}, {b:'bsd'}])).toEqual(2);
+      });
+      
+      it('should handle an array of objects with the same keys correctly', function () {
+        arrayOps = new ArrayOps();        
+        expect(arrayOps.countUnique([{a:'asd'}, {a:'bsd'}])).toEqual(2);
+      });
+      
+      it('should handle an array of objects with the same values correctly', function () {
+        arrayOps = new ArrayOps();        
+        expect(arrayOps.countUnique([{a:'asd'}, {b:'asd'}])).toEqual(2);
+      });
+
     });
   });
 })();
